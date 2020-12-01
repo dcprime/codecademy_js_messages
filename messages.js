552509515e50c80c7cf8dfaa8275e45ec88c983d
Darren Conley <dcprime@gmail.com>
2020-11-30 19:11:26 -0500

    Creating message text

--- messages.js
+++ messages.js
@@ -1,1 +1,9 @@
-console.log('This is a test message');
+const fortune = {
+    youWill: ['encounter a bear', 'kiss a mysterious stranger', 'smack your head', 'regret your life choices', 'discover a new shade of brown', 'clutch your butt cheeks', 'find a new use for meatballs', 'shout YEEEAAAHHH BOYYYY!!!', 'discharge both nostrils', 'narrowly avoid death'],
+
+    after: ['opening a dimensional portal', 'eating an overripe banana', 'pooping', 'making a second piece of toast', 'smelling your own armpits', 'scratching a mosquito bite', 'calculating PI to 12 digits', 'picking your nose', 'drinking from a puddle', 'staring into the face of death'],
+
+    location: ['the Death Star', 'the United States of America', 'a damp cave', 'a parallel dimension', 'the second worst outhouse you\'ve ever seen', 'a truly scary Costco bathroom', 'the grocery store checkout line', '']
+}
+
+console.log('This is your fortune:');
