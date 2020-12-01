f478f47ca51b7fff3ad523ac79fc1f7ee5186c61
Darren Conley <dcprime@gmail.com>
2020-11-30 19:34:36 -0500

    Adding message generation functions

--- messages.js
+++ messages.js
@@ -3,7 +3,16 @@ const fortune = {
 
     after: ['opening a dimensional portal', 'eating an overripe banana', 'pooping', 'making a second piece of toast', 'smelling your own armpits', 'scratching a mosquito bite', 'calculating PI to 12 digits', 'picking your nose', 'drinking from a puddle', 'staring into the face of death'],
 
-    location: ['the Death Star', 'the United States of America', 'a damp cave', 'a parallel dimension', 'the second worst outhouse you\'ve ever seen', 'a truly scary Costco bathroom', 'the grocery store checkout line', '']
+    location: ['the Death Star', 'the United States of America', 'a damp cave', 'a parallel dimension', 'the second worst outhouse you\'ve ever seen', 'a truly scary Costco bathroom', 'the grocery store checkout line', 'a field of sunflowers', 'your bedroom', 'a kitchen that smells like fart'],
+
+    createMessage(){
+        const rand1 = Math.floor(Math.random() * (this.youWill.length + 1));
+        const rand2 = Math.floor(Math.random() * (this.after.length + 1));
+        const rand3 = Math.floor(Math.random() * (this.location.length + 1));
+
+        return `You will ${this.youWill[rand1]} after ${this.after[rand2]} in ${this.location[rand3]}.`
+    }
 }
 
 console.log('This is your fortune:');
+console.log(createMessage());
